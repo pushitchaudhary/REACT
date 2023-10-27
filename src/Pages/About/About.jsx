22a6acf62c26705f964610919459a597c60e1ee9
pushitchaudhary <pushitcoc@gmail.com>
2023-10-27 20:02:07 +0545

    code updated;

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>About Form component</h1>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>This is about</title>
+      </head>
+      <body>
+        <h1>This is about</h1>
+        
+      </body>
+      </html>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>This is home page from components</h1>
+      <p>Add 2 + 2 = {2+2} </p>
     </div>
   )
 }
