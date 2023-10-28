18ad845e75e1f671fa50690b82598a098c74e238
pushitchaudhary <pushitcoc@gmail.com>
2023-10-28 17:14:29 +0545

    hooks -getter ,setter

--- src/Pages/Hookss/Hookss.jsx
+++ src/Pages/Hookss/Hookss.jsx
@@ -3,13 +3,21 @@ import {useState} from 'react'
 
 function Hookss() {
 
+  const [getNumber , setNumber ] = useState(0)
 
-  
+  const increment = ()=>{
+    setNumber(getNumber+1)
+  }
+
+  const descrement = ()=>{
+    setNumber(getNumber)
+  }
 
   return (
     <div>
-      <button style={{height:'60px', width:'100px', fontSize:'30px'}}>+</button>
-      <button  style={{height:'60px',width:'100px', fontSize:'30px', marginLeft:'50px'}}>-</button> 
+      <div style={{fontSize:'40px'}}>{getNumber}</div>
+      <button onClick={increment} style={{height:'60px', width:'100px', fontSize:'30px'}}>+</button>
+      <button onClick={descrement}  style={{height:'60px',width:'100px', fontSize:'30px', marginLeft:'50px'}}>-</button> 
       
     </div>
   )
